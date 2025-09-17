cbccb713c3347a7268ceaebe3f4ebc699c50737e
Riyane El Qoqui <riyane.dev@gmail.com>
2025-09-17 14:02:43 +0200

    feat(termcap): define buffer size and update ft_tgetent function signature

--- include/ft_termcap.h
+++ include/ft_termcap.h
@@ -6,7 +6,7 @@
 /*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/17 13:12:47 by rel-qoqu          #+#    #+#             */
-/*   Updated: 2025/09/17 13:34:26 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2025/09/17 13:57:15 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,9 @@
 
 # include <stdint.h>
 
-int32_t	ft_tgetent(char *bp, const char *name);
+# define FT_TERMCAP_BUFSIZ 4096
+
+int32_t	ft_tgetent(const char *bp, const char *name);
 char	*ft_tgetstr(const char *id, char **area);
 int32_t	ft_tgetnum(const char *id);
 int32_t	ft_tgetflag(const char *id);
