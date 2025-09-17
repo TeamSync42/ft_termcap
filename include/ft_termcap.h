fde74d080e2cbb12697b077bf788def2c7a7835c
Riyane El Qoqui <riyane.dev@gmail.com>
2025-09-17 16:39:26 +0200

    fix(termcap): update ft_tgetflag return type to bool for better clarity and reliability

--- include/ft_termcap.h
+++ include/ft_termcap.h
@@ -6,13 +6,14 @@
 /*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/17 13:12:47 by rel-qoqu          #+#    #+#             */
-/*   Updated: 2025/09/17 13:57:15 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2025/09/17 16:38:31 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_TERMCAP_H
 # define FT_TERMCAP_H
 
+# include <stdbool.h>
 # include <stdint.h>
 
 # define FT_TERMCAP_BUFSIZ 4096
@@ -20,7 +21,7 @@
 int32_t	ft_tgetent(const char *bp, const char *name);
 char	*ft_tgetstr(const char *id, char **area);
 int32_t	ft_tgetnum(const char *id);
-int32_t	ft_tgetflag(const char *id);
+bool	ft_tgetflag(const char *id);
 int32_t	ft_tputs(const char *str, int32_t affcnt, int32_t (*outc)(int32_t));
 char	*ft_tgoto(const char *cap, int32_t row, int32_t col);
 void	ft_termcap_cleanup(void);
--- src/ft_tgetflag.c
+++ src/ft_tgetflag.c
@@ -6,7 +6,7 @@
 /*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/17 16:25:06 by rel-qoqu          #+#    #+#             */
-/*   Updated: 2025/09/17 16:29:22 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2025/09/17 16:38:58 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,22 @@
 #include "core/string/ft_string.h"
 #include "internal/ft_termcap_internal.h"
 
-int32_t	ft_tgetflag(const char *id)
+bool	ft_tgetflag(const char *id)
 {
 	size_t	i;
 
 	if (!g_capabilities || !id)
-		return (0);
+		return (false);
 	i = 0;
 	while (i < g_num_capabilities)
 	{
 		if (ft_strcmp(g_capabilities[i].name, id) == 0)
 		{
 			if (ft_strcmp(g_capabilities[i].value, "1") == 0)
-				return (1);
-			return (0);
+				return (true);
+			return (false);
 		}
 		i++;
 	}
-	return (0);
+	return (false);
 }
